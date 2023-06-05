4cc6f55231176efe0d787cbeac03f9cba144dff8
N.s <100487618+NathanSchalkwijk@users.noreply.github.com>
2023-06-05 18:02:27 +0200

    Update sr.py

--- sr.py
+++ sr.py
@@ -5,36 +5,56 @@ import tkinter as tk
 
 gpio.setmode(gpio.BCM)
 gpio.setup(17, gpio.OUT)
+gpio.setup(18, gpio.OUT)
 gpio.setup(22, gpio.OUT)
+gpio.setup(23, gpio.OUT)
 gpio.setwarnings(False)
 
 
 def init():
     gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT)
+    gpio.setup(18, gpio.OUT)
     gpio.setup(22, gpio.OUT)
+    gpio.setup(23, gpio.OUT)
 
 def forward(tf):
     gpio.output(17, True)
+    gpio.output(18, False)
     gpio.output(22, True)
+    gpio.output(23, False)
     time.sleep(tf)
     gpio.cleanup()
     
 def left(tf):
     gpio.output(17, False)
+    gpio.output(18, True)
     gpio.output(22, True)
+    gpio.output(23, False)
     time.sleep(tf)
     gpio.cleanup()
     
 def right(tf):
     gpio.output(17, True)
+    gpio.output(18, False)
     gpio.output(22, False)
+    gpio.output(23, True)
+    time.sleep(tf)
+    gpio.cleanup()
+    
+def back(tf):
+    gpio.output(17, False)
+    gpio.output(18, True)
+    gpio.output(22, False)
+    gpio.output(23, True)
     time.sleep(tf)
     gpio.cleanup()
     
 def stop(tf):
     gpio.output(17, False)
+    gpio.output(18, False)
     gpio.output(22, False)
+    gpio.output(23, False)
     sys.exit()
     time.sleep(tf)
     
@@ -52,11 +72,15 @@ def key_input(event):
         right(sleep_time)
     elif key_press.lower() == 'q':
         stop(sleep_time)
+    elif key_press.lower() == 's':
+        back(sleep_time)
     else:
         pass
 
 gpio.output(17, False)
+gpio.output(18, False)
 gpio.output(22, False)
+gpio.output(23, False)
 command = tk.Tk()
 command.bind('<KeyPress>', key_input)
 command.mainloop()
