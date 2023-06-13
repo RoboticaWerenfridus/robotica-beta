074286e31bddb3318e064ac7b5e722e1587ed036
N.s <100487618+NathanSchalkwijk@users.noreply.github.com>
2023-06-13 10:56:35 +0200

    Add files via upload

---
+++ bd.py
@@ -0,0 +1,93 @@
+import RPi.GPIO as gpio
+import time
+import sys
+import tkinter as tk
+from bluedot import BlueDot
+
+bd = BlueDot()
+ 
+def init():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(17, gpio.OUT)
+    gpio.setup(18, gpio.OUT)
+    gpio.setup(22, gpio.OUT)
+    gpio.setup(23, gpio.OUT)
+    gpio.setwarnings(False)
+
+init()
+
+def forward(tf):
+    gpio.output(17, True)
+    gpio.output(18, False)
+    gpio.output(22, True)
+    gpio.output(23, False)
+    time.sleep(tf)
+    
+def left(tf):
+    ggpio.output(17, False)
+    gpio.output(18, True)
+    gpio.output(22, True)
+    gpio.output(23, False)
+    time.sleep(tf)
+     
+def right(tf):
+    gpio.output(17, True)
+    gpio.output(18, False)
+    gpio.output(22, False)
+    gpio.output(23, True)
+    time.sleep(tf)
+    
+def back(tf):
+    gpio.output(17, False)
+    gpio.output(18, True)
+    gpio.output(22, False)
+    gpio.output(23, True)
+    time.sleep(tf)
+    gpio.cleanup()
+    
+def stop(tf):
+    gpio.output(4, False)
+    gpio.output(5, False)
+    sys.exit()
+    time.sleep(tf)
+    
+def move(pos):
+    init()
+    sleep_time = 0.030
+    
+    if pos.top:
+        forward(sleep_time)
+    elif pos.left:
+        left(sleep_time)
+    elif pos.right:
+        right(sleep_time)
+    elif pos.bottom:
+        back(sleep_time)
+
+def init():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(17, gpio.OUT)
+    gpio.setup(18, gpio.OUT)
+    gpio.setup(22, gpio.OUT)
+    gpio.setup(23, gpio.OUT)
+    gpio.setwarnings(False)
+    
+def stop1():
+    init()
+    gpio.output(17, False)
+    gpio.output(18, False)
+    gpio.output(22, False)
+    gpio.output(23, False)
+
+gpio.output(17, False)
+gpio.output(18, False)
+gpio.output(22, False)
+gpio.output(23, False)
+bd.when_pressed = move
+bd.when_moved = move
+bd.when_released = stop1
+command = tk.Tk()
+command.mainloop()
+
+        
+
