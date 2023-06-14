72cfad85092c657b6a0d2cdf2fe95a5cab57e300
N.s <100487618+NathanSchalkwijk@users.noreply.github.com>
2023-06-14 07:37:00 +0200

    Create i.py

---
+++ i.py
@@ -0,0 +1,30 @@
+import subprocess
+
+def install_library(library_name):
+
+    try:
+
+        # Check if the library is already installed
+
+        __import__(library_name)
+
+        print(f"{library_name} is already installed.")
+
+    except ImportError:
+
+        # Install the library using pip
+
+        print(f"Installing {library_name}...")
+
+        subprocess.check_call(['pip', 'install', library_name])
+
+        print(f"{library_name} has been successfully installed.")
+
+# Install bluedot library
+
+install_library('bluedot')
+
+# Install tkinter library
+
+install_library('tk')
+
