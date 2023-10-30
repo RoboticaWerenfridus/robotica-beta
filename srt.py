42b4f4c6f2f5a8f988245a26e76498ebc162ee5f
N.s <100487618+NathanSchalkwijk@users.noreply.github.com>
2023-10-30 15:29:01 +0100

    Update srt.py

--- srt.py
+++ srt.py
@@ -11,11 +11,11 @@ gpio.setup(17, gpio.OUT)
 gpio.setup(18, gpio.OUT)
 gpio.setup(22, gpio.OUT)
 gpio.setup(23, gpio.OUT)
-gpio.setup(4, gpio.OUT)
+gpio.setup(21, gpio.OUT)
 
 def init():
     gpio.setmode(gpio.BCM)
-    gpio.setup(4, gpio.OUT)
+    gpio.setup(21, gpio.OUT)
     gpio.setup(17, gpio.OUT)
     gpio.setup(18, gpio.OUT)
     gpio.setup(22, gpio.OUT)
@@ -62,9 +62,9 @@ def stop(tf):
     time.sleep(tf)
 
 def toeter(tf):
-    gpio.output(4, True)
+    gpio.output(21, True)
     time.sleep(tf)
-    gpio.output(4, False)
+    gpio.output(21, False)
     
 def key_input(event):
     init()
@@ -91,7 +91,7 @@ gpio.output(17, False)
 gpio.output(18, False)
 gpio.output(22, False)
 gpio.output(23, False)
-gpio.output(4, False)
+gpio.output(21, False)
 command = tk.Tk()
 command.bind('<KeyPress>', key_input)
 command.mainloop()
