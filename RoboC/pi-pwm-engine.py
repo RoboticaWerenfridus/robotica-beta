5b7881d2104d9d04a1351bd2fee5a739cdae0ae2
N.s <100487618+NathanSchalkwijk@users.noreply.github.com>
2025-01-06 15:33:45 +0100

    Update pi-pwm-engine.py

--- RoboC/pi-pwm-engine.py
+++ RoboC/pi-pwm-engine.py
@@ -4,10 +4,10 @@ import sys
 import pygame
 
 gpio.setmode(gpio.BCM)
-gpio.setup(17, gpio.OUT)
-gpio.setup(18, gpio.OUT)
-gpio.setup(22, gpio.OUT)
-gpio.setup(23, gpio.OUT)
+gpio.setup(17, gpio.OUT)  # Motor 1 forward
+gpio.setup(18, gpio.OUT)  # Motor 1 backward
+gpio.setup(22, gpio.OUT)  # Motor 2 forward
+gpio.setup(23, gpio.OUT)  # Motor 2 backward
 gpio.setwarnings(False)
 
 motor1_forward_pwm = gpio.PWM(17, 100)
@@ -34,10 +34,47 @@ joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 max_speed = 100
+button_actions = {
+    0: "X",  # X button
+    3: "Triangle",  # Triangle button
+    1: "Circle",  # Circle button
+    2: "Square",  # Square button
+    10: "AL", #Analog button left
+}
 
+# Define functions for each button action
+def action_x():
+    print("X button pressed! Executing action X.")
+    # Add custom behavior here for X button
+
+def action_triangle():
+    print("Triangle button pressed! Executing action Triangle.")
+    # Add custom behavior here for Triangle button
+
+def action_circle():
+    print("Circle button pressed! Executing action Circle.")
+    # Add custom behavior here for Circle button
+
+def action_square():
+    print("Square button pressed! Executing action Square.")
+    # Add custom behavior here for Square button
+
+def action_al():
+    print("Analog button left pressed! Executing action AL")
+# Mapping buttons to their respective actions
+
+button_function_mapping = {
+    0: action_x,
+    1: action_circle,
+    2: action_square,
+    3: action_triangle,
+    10: action_al,
+}
+
+# Start the main loop
 try:
     while True:
-        pygame.event.pump()
+        pygame.event.pump()  # Update the joystick events
         axis_x = joystick.get_axis(0)  # Get X-axis value of joystick
         axis_y = -joystick.get_axis(1)  # Get Y-axis value of joystick and invert
         
@@ -61,6 +98,11 @@ try:
         else:
             set_motor_speed(motor2_forward_pwm, 0)
             set_motor_speed(motor2_backward_pwm, speed_motor2_dc)
+
+        # Check if any button is pressed and execute the corresponding function
+        for button_id in range(5):  # Only check X, Circle, Square, Triangle buttons
+            if joystick.get_button(button_id):
+                button_function_mapping.get(button_id, lambda: print(f"Button {button_id} pressed!"))()
         
         time.sleep(0.05)
 
